d4829600ef6b5e2d39b42628687e06ad1be5e71b
Kimmo Sääskilahti <ksaaskil@gmail.com>
2022-10-01 15:10:17 +0300

    Add app/.

---
+++ app/coacher/config.py
@@ -0,0 +1,9 @@
+import os
+
+POSTGRES_CONFIG = {
+    "username": os.environ.get("POSTGRES_USER", "postgres"),
+    "db_name": os.environ.get("POSTGRES_DB", "coacher"),
+    "host": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+    "password": os.environ.get("POSTGRES_PASSWORD", ""),
+    "port": os.environ.get("POSTGRES_PORT", 5432),
+}
---
+++ app/status/views.py
@@ -0,0 +1,10 @@
+from django.db import connection
+from django.http import JsonResponse
+
+def index(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1")
+        return JsonResponse({ "message": "OK!"}, status=200)
+    except Exception as ex:
+        return JsonResponse({ "error": str(ex) }, status=500)
